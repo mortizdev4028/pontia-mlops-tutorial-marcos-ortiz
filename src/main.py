f5b077c901716d1f081caa0be2489f4ee1b35493
Marcos Ortiz <mortiz.dev4028@gmail.com>
2025-11-14 02:22:41 +0100

    Second Commit: Add new files (requirements) and modified files (main.py, .gitignore)

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ import mlflow
 import mlflow.sklearn
 from pathlib import Path
 from datetime import datetime
-from src.data_loader import load_data, preprocess_data
-from src.evaluate import evaluate
-from src.model import train_model
+from data_loader import load_data, preprocess_data
+from evaluate import evaluate
+from model import train_model
 
 # Configurar logging (consola + archivo)
 logging.basicConfig(
@@ -24,8 +24,8 @@ logger=logging.getLogger("adult-income")
 run_name = f"run-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
 
 # MLflow config
-MLFLOW_URI = "http://localhost:5000"
-EXPERIMENT_NAME = "adult-income"
+MLFLOW_URI = "http://mlflow-9675.eastus.azurecontainer.io:5000"
+EXPERIMENT_NAME = "adult-income-mortiz"
 
 mlflow.set_tracking_uri(MLFLOW_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
